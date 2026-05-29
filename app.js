168c18ffd45e3fde2116101d17188a5862ddd2f6
altaris33 <jeremie.laera@live.fr>
2026-05-30 01:40:49 +0200

    Understanding http request and writing manual http responses - wrote a simple HTML response

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
-const server = http.createServer((res, req) => {
-    console.log(req);
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+
+    // header (meta-data) for the response
+    res.setHeader('Content-Type', 'text/html');
+
+    // response that Node will send back to the client
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my Node.js server.</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
