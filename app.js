1760ba530a98547ae4a606a23e50757c236b97d7
altaris33 <jeremie.laera@live.fr>
2026-06-01 18:00:34 +0200

    Parsing a request body and writing it to a file

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const fs = require('fs');
 
+// This is the raw logic for creating an HTTP server
+// Reading and parsing a request and writing a response
+// Express JS will automatically help us write less raw code for this purpose
 const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
@@ -14,7 +17,23 @@ const server = http.createServer((req, res) => {
     }
 
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        // Get the request data
+        // Register to an event listener -> we listen to an event called data event
+        // This event will start whenver a new chunk of data is ready to be read
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        // Once all chunks have been read, we register to another event called 'end'
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            console.log(parsedBody);
+            fs.writeFileSync('message.txt', message);
+        });
+    
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
